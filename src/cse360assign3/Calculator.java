f8b55cdef9a0571bbaacb85bb40f8b73e4e9c65f
Denise <dbperry2@asu.edu>
2016-02-17 16:26:39 -0700

    Initial version

---
+++ src/cse360assign3/Calculator.java
@@ -0,0 +1,88 @@
+
+package cse360assign3;
+/**
+ * Creates a calculator that allows for adding, subtracting, multiplying, and dividing.
+ *  
+ * @author	Denise Perry
+ * @version	02/17/2016
+ */
+
+public class Calculator
+{
+	private int total;
+	
+	
+	/**
+	 * Creates a calculator object
+	 */
+	public Calculator ()
+	{
+		total = 0;  // not needed - included for clarity
+	}
+
+	
+	/**
+	 * getTotal - gets the total 
+	 * 
+	 * @return The total (NOTE TO SELF: edit this as well)
+	 */
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	
+	/**
+	 * add - adds an integer to the current total 
+	 * 
+	 * @param value	The term to be added
+	 */
+	public void add (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * subtract - subtracts an integer from the current total
+	 * 
+	 * @param value	The term to be subtracted from the total
+	 */
+	public void subtract (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * multiply - multiplies an integer with the current total
+	 * 
+	 * @param value	The number to be multiplied with the total
+	 */
+	public void multiply (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * divide - divides the total by an integer
+	 * 
+	 * @param value	The divisor
+	 */
+	public void divide (int value)
+	{
+		
+	}
+	
+	
+	/**
+	 * getHistory - Shows prior operations done by the user
+	 * 
+	 * @return	A string showing operations done by the user
+	 */
+	public String getHistory ()
+	{
+		return "";
+	}
+}
